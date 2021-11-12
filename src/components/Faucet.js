b89c89b60c091da9e1680f23cb65ebc66578e743
ethdumplingz <94034316+ethdumplingz@users.noreply.github.com>
2021-11-12 18:04:06 -0500

    missed a new hardcoded network name references

--- src/components/Faucet.js
+++ src/components/Faucet.js
@@ -63,8 +63,8 @@ const Faucet = (props) => {
 			</Card>
 			<Card style={{background: "rgb(209, 213, 219)", padding: '30px'}}>
 				<Card.Body>
-					<Card.Title style={{fontWeight: 600,fontSize:'1.5em'}}>Make it rain <b>(Testnet)</b> $FISH</Card.Title>
-					<Card.Subtitle style={{margin: "30px 0px", lineHeight: 1.5}}>Click below to receive your <b>$FISH</b> for development on <b>Ropsten Testnet</b>.</Card.Subtitle>
+					<Card.Title style={{fontWeight: 600,fontSize:'1.5em'}}>Make it rain <b>({network} Testnet)</b> $FISH</Card.Title>
+					<Card.Subtitle style={{margin: "30px 0px", lineHeight: 1.5}}>Click below to receive your <b>$FISH</b> for development on <b>{network} Testnet</b>.</Card.Subtitle>
 					<div className="d-grid gap-2" style={{marginBottom:"15px"}}>
 						<Button onClick={faucet} variant={"primary faucet-btn"}>Make it 💦  $FISH!</Button>
 					</div>
